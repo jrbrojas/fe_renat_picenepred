bf82ca2e18a00e5bab38ef0941f0669c685328d7
Erick Tucto <erick@ericktucto.com>
2025-10-02 11:34:11 -0500

    compilacion correcta del proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //server: {
-  //  host: '0.0.0.0',
-  //  hmr: {
-  //    //clientPort: 5173,
-  //    host: '192.168.0.3',
-  //  },
-  //},  
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      //clientPort: 5173,
-      host:'127.0.0.1:3000'
-      //host: '192.168.2.94',
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            //clientPort: 5173,
+            host: '127.0.0.1:3000'
+            //host: '192.168.2.94',
+        },
     },
-  },  
-  plugins: [react()],
-  base: '/tablero',
+    plugins: [react()],
 })
